b21c46f00e04128ffb3e43cb2ebc01641bdc2c7d
Michaeldam88 <michaeldam88@gmail.com>
2022-10-30 12:29:56 +0100

    adeed the funtion

---
+++ index.js
@@ -0,0 +1,17 @@
+import { strictEquals } from './js/strictEquals';
+
+console.table([
+  ['A', 'B', 'RESULT'],
+  [1, 1, strictEquals(1, 1)],
+  [NaN, NaN, strictEquals(NaN, NaN)],
+  [0, -0, strictEquals(0, -0)],
+  [-0, 0, strictEquals(-0, 0)],
+  [1, '1', strictEquals(1, '1')],
+  [true, false, strictEquals(true, false)],
+  [false, true, strictEquals(NaN, NaN)],
+  ['water', 'oil', strictEquals(false, true)],
+  [null, null, strictEquals(null, null)],
+  [undefined, undefined, strictEquals(undefined, undefined)],
+  [15n, 15, strictEquals(15n, 15)],
+  [15n, 15n, strictEquals(15n, 15n)],
+]);
---
+++ js/strictEquals.js
@@ -0,0 +1,19 @@
+const exceptionNaN = [NaN];
+const exception0 = [0];
+const exceptionNEgative0 = [-0];
+
+export const strictEquals = (a, b) => {
+  if (exceptionNaN.includes(a) && exceptionNaN.includes(b)) {
+    return !Object.is(a, b);
+  }
+
+  if (exception0.includes(a) && exceptionNEgative0.includes(b)) {
+    return !Object.is(a, b);
+  }
+
+  if (exceptionNEgative0.includes(a) && exception0.includes(b)) {
+    return !Object.is(a, b);
+  }
+  return Object.is(a, b);
+};
+export default strictEquals;
